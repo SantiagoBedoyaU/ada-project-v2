c10fdf58132eb75362815ca5048dad30a7ffdd39
SantiagoBedoyaU <santiago.bedoya35419@ucaldas.edu.co>
2024-10-30 17:09:52 -0500

    tensor_product

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 
-
 def load_tpm(filename_tpm: str, num_elements: int):
     states = pd.Index(
         [np.binary_repr(i, width=num_elements)[::-1] for i in range(2**num_elements)]
@@ -14,8 +13,8 @@ def load_tpm(filename_tpm: str, num_elements: int):
 
 def apply_background(df_tpm, initial_state, candidate_system):
     background_condition = {
-        idx: initial_state[idx] 
-        for idx, bit in enumerate(candidate_system) 
+        idx: initial_state[idx]
+        for idx, bit in enumerate(candidate_system)
         if bit == "0"
     }
     filtered_states = [
@@ -58,11 +57,14 @@ def marginalize_cols(df_tpm, future_subsystem: str):
     return result_df.T
 
 
-def apply_marginalize(df_tpm, elements):
-    df_tpm_marginalize_rows = marginalize_rows(df_tpm, elements)
-    df_marginalized = marginalize_cols(df_tpm_marginalize_rows, elements)
-    return df_marginalized
-
+def tensor_product(df1: pd.DataFrame, df2: pd.DataFrame):
+    result = pd.DataFrame()
+    for df2col in df2.columns:
+        for df1col in df1.columns:
+            name = f"{df1col}{df2col}"
+            result[name] = df1[df1col] * df2[df2col]
+    
+    return result
 
 [initial_state_str, candidate_system_str, present_subsystem_str, future_subsystem_str] = np.loadtxt(
     "system_values.csv", delimiter=",", skiprows=1, dtype=str
@@ -78,8 +80,15 @@ df_tpm, states = load_tpm("matrix_guia.csv", len(candidate_system))
 result_df = apply_background(df_tpm, initial_state, candidate_system)
 # print(result_df)
 
-result_df = marginalize_rows(result_df, present_subsystem)
+# result_df = marginalize_rows(result_df, present_subsystem)
 # print(result_df)
 
-result_df = marginalize_cols(result_df, future_subsystem)
-print(result_df)
+df_a = marginalize_cols(result_df, '1000')
+df_b = marginalize_cols(result_df, '0100')
+df_c = marginalize_cols(result_df, '0010')
+
+result_ab = tensor_product(df_a, df_b)
+result_abc = tensor_product(result_ab, df_c)
+print(result_abc)
+# result_abc = tensor_product(result_ab, df_c)
+# print(result_abc)
