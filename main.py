fa2080910a564706de7570496769f7fe8727a8be
SantiagoBedoyaU <santiago.bedoya35419@ucaldas.edu.co>
2024-10-30 15:38:42 -0500

    marginalize_rows

--- main.py
+++ main.py
@@ -12,9 +12,11 @@ def load_tpm(filename_tpm: str, num_elements: int):
     return df_tpm, states
 
 
-def apply_background(df_tpm, candidate_system):
+def apply_background(df_tpm, initial_state, candidate_system):
     background_condition = {
-        idx: "0" for idx, bit in enumerate(candidate_system) if bit == "0"
+        idx: initial_state[idx] 
+        for idx, bit in enumerate(candidate_system) 
+        if bit == "0"
     }
     filtered_states = [
         state
@@ -25,18 +27,19 @@ def apply_background(df_tpm, candidate_system):
     return result_df
 
 
-def marginalize_rows(df_tpm, var_to_marginalize):
-    var_positions = {'A': 0, 'B': 1, 'C': 2, 'D': 3}
-    all_vars = set('ABCD')
-    vars_to_keep = all_vars - set(var_to_marginalize)
-    positions_to_keep = [var_positions[var] for var in vars_to_keep]
+def marginalize_rows(df_tpm, present_subsystem: str):
+    n_bits = len(df_tpm.index[0])
+    if len(present_subsystem) != n_bits:
+        raise ValueError("invalid present subsystem")
+
+    positions_to_keep = [i for i, bit in enumerate(present_subsystem) if bit == '1']
 
     def extract_bits(binary_str, positions):
         return ''.join([binary_str[i] for i in positions])
 
     new_index = df_tpm.index.map(lambda x: extract_bits(x, positions_to_keep))
-    result_df = df_tpm.groupby(new_index).mean()
 
+    result_df = df_tpm.groupby(new_index).mean()
     return result_df
 
 def marginalize_cols(df_tpm, elements):
@@ -54,21 +57,22 @@ def apply_marginalize(df_tpm, elements):
     return df_marginalized
 
 
-[initial_state_str, candidate_system_str] = np.loadtxt(
+[initial_state_str, candidate_system_str, present_subsystem_str, future_subsystem_str] = np.loadtxt(
     "system_values.csv", delimiter=",", skiprows=1, dtype=str
 )
 initial_state = initial_state_str.strip()
 candidate_system = candidate_system_str.strip()
+present_subsystem = present_subsystem_str.strip()
+future_subsystem = future_subsystem_str.strip()
 
 df_tpm, states = load_tpm("matrix_guia.csv", len(candidate_system))
 # print(df_tpm)
 
-result_df = apply_background(df_tpm, candidate_system)
-print(result_df)
-print()
-
-result_df = marginalize_rows(result_df, 'BCD')
-print(result_df)
-# result_df = marginalize_rows(result_df, [1, 2, 3])
+result_df = apply_background(df_tpm, initial_state, candidate_system)
 # print(result_df)
+
+# ABCDE = 5
+result_df = marginalize_rows(result_df, present_subsystem)
+print(result_df)
+
 # result_df = marginalize_rows(result_df, [])
